727abbc23679024ba4fdcaf4a83d01585f19968b
Deep <deep.patel23@outlook.com>
2022-07-15 14:25:21 -0600

    Added new logo and adding navbar now

---
+++ src/components/layouts/logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div>
+        <img src={require("./logo.png")} alt="ReFinanced-Logo"/>
+    </div>
+  );
+}
+  
+export default Logo;
---
+++ src/components/layouts/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <h1>Hello Who is this?</h1>
+      </header>
+    </div>
+  );
+}
+  
+  export default Navbar;
+  

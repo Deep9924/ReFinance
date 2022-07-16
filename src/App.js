1c3618b68e0c8e07cc507525fb26118558cd3072
Deep9924 <91365008+Deep9924@users.noreply.github.com>
2022-07-15 21:29:11 -0600

    Update App.js
    Delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {React} from 'react'; //, useState
-import Navbar from './components/layouts/navbar';
+//import Navbar from './components/layouts/navbar';
 import Logo from './components/layouts/logo';
 
 

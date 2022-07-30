7382a238442681a77d9be9599bfd3f5e6b19f816
Deep Patel <deep.patel23@outlook.com>
2022-07-30 00:36:49 -0600

    V

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-//const express = require("express");
+//import express from 'express';
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Trying"));
 
 app.listen(port, () => console.log(`Server is listening on: ${port} `));

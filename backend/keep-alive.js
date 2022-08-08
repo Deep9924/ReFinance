7115bae7347b133f3efb28e7a9fcdbccea295551
Deep <deep.patel23@outlook.com>
2022-08-08 09:28:47 -0600

    ALIVE

--- backend/keep-alive.js
+++ backend/keep-alive.js
@@ -1,3 +1,3 @@
 const fetch = require('node-fetch');
-
+// setInterval(async() => await fetch(`https://refinanced.herokuapp.com`), 5 * 60 * 1000);
 setInterval(() => fetch(`https://refinanced.herokuapp.com`), 5 * 60 * 1000);

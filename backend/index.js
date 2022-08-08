706102cb2f9d56f2dcbbe85c8600e90616fe37b3
Deep <deep.patel23@outlook.com>
2022-08-08 09:25:21 -0600

    ALIVE

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.get('/index', (req,res) => {
     res.json(val);
 })
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
 
--- backend/keep-alive.js
+++ backend/keep-alive.js
@@ -1,3 +1,3 @@
 const fetch = require('node-fetch');
 
-setInterval(() => fetch(`https://<your_app>.herokuapp.com`), 5 * 60 * 1000);
+setInterval(() => fetch(`https://refinanced.herokuapp.com`), 5 * 60 * 1000);

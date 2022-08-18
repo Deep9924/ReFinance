0c3a1dcdf44938af85bb7d81f587f7d9a719e84f
Deep <deep.patel23@outloook.com>
2022-08-18 14:58:51 -0600

    Db

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+// const path = require("path");
 const axios = require("axios");
 const cors = require('cors');
 const app = express();
@@ -7,6 +7,7 @@ app.use(cors());
 const port = process.env.PORT || "5000";
 const data = require("./nasdaqD");
 const indexScroll = require("./indexScroll");
+const db = require('./database');
 require('dotenv').config();
 require('./keep-alive');
 
@@ -38,6 +39,16 @@ app.get('/indices', (req, res) => {
 app.get('/index', (req,res) => {
     res.json(indexScroll);
 })
+
+app.get('/db', (req, res) => {
+  db.query("SELECT * FROM User", (err, result) =>{
+    if (err){
+      console.log(err);
+    }
+
+    res.send(result);
+  });
+});
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);

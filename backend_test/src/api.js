4802fff42fe4f555582973d7213eaffb56d83d96
Deep <deep.patel23@outloook.com>
2022-08-25 14:19:31 -0600

    V

--- backend_test/src/api.js
+++ backend_test/src/api.js
@@ -1,91 +1,16 @@
 const express = require("express");
-// const path = require('path');
-const axios = require("axios");
-const cors = require("cors");
-const app = express();
-app.use(cors());
-const port = process.env.PORT || "5000";
-const data = require("./nasdaqD");
-const indexScroll = require("./indexScroll");
-const graph_data = require("./graph_data");
-const pool = require("./database");
-require("dotenv").config();
-require("./keep-alive");
-
 const serverless = require("serverless-http");
+const app = express();
 const router = express.Router();
 
-app.get("/", (req, res) => {
-  res.status(200).send("WHATABYTE: Food For Devs");
-});
 
-app.get("/search", (req, res) => {
-  const searchInput = req.query.q;
-
-  const newFilter = data.filter((value) => {
-    const regex = new RegExp(`^${searchInput}`, "gi");
-    return value.Symbol.match(regex) || value.Name.match(regex);
-  });
-
-  res.send({ q: newFilter.slice(0, 10) });
-});
-
-app.get("/indices", (req, res) => {
-  const symbol_quote = `https://api.twelvedata.com/quote?symbol=SPX,IXIC,DJIA,RUT,COMP,BTC/USD,ETH/USD&apikey=${process.env.STOCK_TOKEN}`;
-  axios
-    .get(symbol_quote)
-    .then((response) => {
-      const data = response.data;
-      res.json(data);
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/index", (req, res) => {
-  res.json(indexScroll);
-});
-
-app.get("/graph", async (req, res) => {
-  const graph_input = req.query.id;
-  const query = await pool.query(
-    "SELECT * FROM stock WHERE symbol_ticker = $1",
-    [graph_input]
-  );
-
-  //console.log(l.rows)
-  res.send(query.rows);
-});
-
-app.get("/SPX", async (req, res) => {
-  const graph_input = "APPL";
-  const query = await pool.query(
-    "SELECT * FROM stock WHERE symbol_ticker = $1",
-    [graph_input]
-  );
-
-  //console.log(l.rows)
-  res.send(query.rows);
-});
-
-/* app.get('/gp', (req,res) => {
-  res.json(graph_data);
-})  */
-
-app.get("/db", (req, res) => {
-  //pool.query("INSERT INTO links (url, name) VALUES('helloasd','O''Reilly Media');")
-  pool.query("SELECT	* FROM stock;", (error, response) => {
-    //console.log( response.rows);
-    res.send(response.rows);
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi",
   });
 });
 
-/* 
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-});
- */
-
-app.use(`/.netlify/functions/api`, router);
+app.use("/.netlify/functions/api", router);
 
 module.exports = app;
 module.exports.handler = serverless(app);

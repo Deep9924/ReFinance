e15d5076404cd3278214a43896562c823b2c1c0d
Deep <deep.patel23@outloook.com>
2022-12-09 15:36:12 -0600

    TEST

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ import { AuthProvider } from "./firebase/AuthContext";
 import PrivateRoute from "./components/layouts/PrivateRouter.js/PrivateRoute";
 import { AddStock, RemoveStock, AddNews, RemoveNews } from "./components/layouts/admin";
 import NotFound from "./components/layouts/notFound/NotFound";
-import { ProfileProvider } from "./components/layouts/context/ProfileContext";
 //import { FavouritesContext } from "./components/layouts/context/FavouritesContext";
 
 function App() {
@@ -17,46 +16,44 @@ function App() {
 		<div className='App'>
 			<Router>
 				<AuthProvider>
-					<ProfileProvider>
-						<Navbar />
-						<div className='main-body'>
-							<Container>
-								<div className='navigation'>
-									<Routes>
-										<Route exact path='/' element={<Home />} />
-										<Route path='/favourites' element={<Favourites />} />
-										<Route path='/stock' element={<Navigate to='/notfound' />} />
-										<Route path='/stock/:symbol' element={<Stock />} />
-										<Route path='/contact' element={<Contact />} />
-										<Route
-											path='/login'
-											element={
-												<PrivateRoute>
-													{" "}
-													<Login />{" "}
-												</PrivateRoute>
-											}
-										/>
-										<Route
-											path='/signup'
-											element={
-												<PrivateRoute>
-													{" "}
-													<SignUp />{" "}
-												</PrivateRoute>
-											}
-										/>
-										<Route path='/addstock' element={<AddStock />} />
-										<Route path='/removestock' element={<RemoveStock />} />
-										<Route path='/addnews' element={<AddNews />} />
-										<Route path='/removenews' element={<RemoveNews />} />
-										<Route path='/notfound' element={<NotFound />} />
-										<Route path='/*' element={<Navigate to='/notfound' />} />
-									</Routes>
-								</div>
-							</Container>
-						</div>
-					</ProfileProvider>
+					<Navbar />
+					<div className='main-body'>
+						<Container>
+							<div className='navigation'>
+								<Routes>
+									<Route exact path='/' element={<Home />} />
+									<Route path='/favourites' element={<Favourites />} />
+									<Route path='/stock' element={<Navigate to='/notfound' />} />
+									<Route path='/stock/:symbol' element={<Stock />} />
+									<Route path='/contact' element={<Contact />} />
+									<Route
+										path='/login'
+										element={
+											<PrivateRoute>
+												{" "}
+												<Login />{" "}
+											</PrivateRoute>
+										}
+									/>
+									<Route
+										path='/signup'
+										element={
+											<PrivateRoute>
+												{" "}
+												<SignUp />{" "}
+											</PrivateRoute>
+										}
+									/>
+									<Route path='/addstock' element={<AddStock />} />
+									<Route path='/removestock' element={<RemoveStock />} />
+									<Route path='/addnews' element={<AddNews />} />
+									<Route path='/removenews' element={<RemoveNews />} />
+									<Route path='/notfound' element={<NotFound />} />
+									<Route path='/*' element={<Navigate to='/notfound' />} />
+								</Routes>
+							</div>
+						</Container>
+					</div>
 				</AuthProvider>
 			</Router>
 		</div>
